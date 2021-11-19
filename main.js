a6bd06077cd2c8b6c26a79df9d041f508d551b0c
majdjb7 <majdjd171@gmail.com>
2021-11-19 19:12:20 +0200

    Added Extension Feature #1

--- main.js
+++ main.js
@@ -9,7 +9,22 @@ $("#loadData").on("click", function () {
 
 $("#displayData").on("click", function () {
   const render = new Renderer(apiManager.data);
-  // console.log(apiManager.data)
   render.renderResults();
 });
 
+
+
+$("#saveUserPage").on("click", function () {
+  localStorage.clear()
+  let userToSave = JSON.stringify(apiManager.data)
+  localStorage['firstUser'] = userToSave;
+  
+  
+});
+
+$("#loadUserPage").on("click", function () {
+  let userToLoad = JSON.parse(localStorage.firstUser)
+  const render = new Renderer(userToLoad);
+  render.renderResults();
+  
+});
